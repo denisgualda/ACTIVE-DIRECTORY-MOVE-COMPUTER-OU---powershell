42d6e13a29f81d56ab2e11c0023581f468a3ef0c
denisgualda <49091329+denisgualda@users.noreply.github.com>
2020-11-26 13:39:56 +0100

    Add files via upload

--- MOURE_EQUIPS_OU.ps1
+++ MOURE_EQUIPS_OU.ps1
@@ -1,9 +1,16 @@
-$computers = Get-Content C:\llistat.txt
+$computers = Get-Content C:\MOURE_EQUIP_OU_powershell\llistat.txt
 
 # Specify the path to the OU where computers will be moved.
-$TargetOU   = "OU=grupadm02,OU=PRE_administracio,DC=domini,DC=lab" 
+#$TargetOU   = "OU=NOUS,OU=BECARIS,OU=ADMINISTRACIO_GRUP1,OU=SECTOR_ADMINISTRACIO,DC=domini,DC=lab" 
+Write-Host " "
+$TargetOU = Read-Host "Introdueix UO desti "
 ForEach( $computer in $computers){
     Get-ADComputer $computer |
     Move-ADObject -TargetPath $TargetOU
-
 }
+Write-Host " "
+Write-Host "-------------------------------"
+Write-Host " "
+Write-Host " Equips moguts a la nova OU: $TargetOu " 
+Write-Host " "
+Write-Host "-------------------------------"
